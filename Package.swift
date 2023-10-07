05dc32c69fd7dd7cc6e27740f44cfa76072aa2d2
yangliu-1995 <yangliu-1995@github.com>
2023-10-07 09:31:36 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -13,13 +13,13 @@ let package = Package(
             ]),
     ],
     targets: [
-        .binaryTarget(name: "ffmpegkit", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/ffmpegkit.zip", checksum: "8e14767138e78bc3bce6c6575426c50d4cfda262fab705591458a0f6150b5875"),
-        .binaryTarget(name: "avcodec", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/libavcodec.zip", checksum: "54580c8b5f16930f3505718744862ae2b1ddd532182b3a3c83f4515454c8447d"),
-        .binaryTarget(name: "avdevice", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/libavdevice.zip", checksum: "4a57da7dfa4be6f098f19bc485c9b3480ff3d36fc85d05a31c27360a476da4da"),
-        .binaryTarget(name: "avfilter", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/libavfilter.zip", checksum: "cdf8ccf82b949629bfab6d934555b325b6ee533d621f70792093e2ba3ff872b6"),
-        .binaryTarget(name: "avformat", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/libavformat.zip", checksum: "7482b50ab2a63590e79e4e02d42fc317c3a13169d5da0c7ddcbaaa0fdfdd3509"),
-        .binaryTarget(name: "avutil", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/libavutil.zip", checksum: "78c8d9c187485148c0d1238d4e4c33743843827f1d0980be08b562ab986a5e41"),
-        .binaryTarget(name: "swresample", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/libswresample.zip", checksum: "5f30e6a7ffd97d85a8605908be5d8b99a0cadf5da8d277c9729c8f1dd00aec68"),
-        .binaryTarget(name: "swscale", url: "https://github.com/yangliu-1995/FFMpegKit-SPM/releases/download/1.0/libswscale.zip", checksum: "618c85c415224841e2b16f15adc27dd7207b137e11c35b05f135298eeb01da50"),
+        .binaryTarget(name: "ffmpegkit", url: "https://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/ffmpegkit.zip", checksum: "f53ef9e77fb931562436c8e2ec364a784921c8bc3f97adbedd6e7abfc8d74315"),
+        .binaryTarget(name: "avcodec", url: "hhttps://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/libavcodec.zip", checksum: "95cf224c7a18e2317bf16d4c76133b7eb80375e58f517265920eb1ef9bb10102"),
+        .binaryTarget(name: "avdevice", url: "https://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/libavdevice.zip", checksum: "52b9921f7fa7125017d5a3b89b9ad47163ebf73282345048b7d03db630735abe"),
+        .binaryTarget(name: "avfilter", url: "https://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/libavfilter.zip", checksum: "70ecbcfa02d24d590ac42c0c64e51ca4f7dd22eedeeea78dc4026302d8485970"),
+        .binaryTarget(name: "avformat", url: "https://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/libavformat.zip", checksum: "1a5d2cf1d4c5a0fa9fb3bb72929eecbfcd63f78689442a5b66ed41cc1bfdb2f2"),
+        .binaryTarget(name: "avutil", url: "https://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/libavutil.zip", checksum: "98cc8ce652043db7fb348902baedf508c72017234c19311e86d73101ac6125a3"),
+        .binaryTarget(name: "swresample", url: "https://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/libswresample.zip", checksum: "d98803ba0b7d753eb644a1e5aa40ef57603fffe3a08922787bdf0e7ec1f9e2bf"),
+        .binaryTarget(name: "swscale", url: "https://github.com/yangliu-1995/FFmpegKit/releases/download/0.0.1/libswscale.zip", checksum: "ccb51858b087607f65c4059b56c1847067199d56c7d5ca8870d1c80c0df59a55"),
     ]
 )
